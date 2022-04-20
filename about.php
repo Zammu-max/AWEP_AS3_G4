9d3d55a71e4e0fd5a2a9599270449cf8ba36bb98
Muazzam <20FTT1443@student.pb.edu.bn>
2022-04-20 14:09:31 +0800

    Initial commit

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="css/about.css">
+<title>About Us</title>
+<!-- LINK IS REFFERED FROM W3SCHOOLS.COM, TITLE AS HOW TO CREATE ICON BUTTONS, https://www.w3schools.com/howto/howto_css_icon_buttons.asp -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- BIZKID LOGO IS REFFERED FROM CANVA.COM, TITLE AS KIDS ZONE LOGO, https://www.canva.com/design/DAE-THhYchc/fJybr9urk51Ori8mV7F8Ag/edit?layoutQuery=kids+logo -->
+
+<html>
+
+<body>
+    <div class="wrap">
+		<div class="side">
+		<img src="images/bizkid2.png" id="bizkid">
+        <a class="sideBar" style="margin-top: 20px;" href="main.php">Home</a>
+        <a class="sideBar" style="margin-top: 20px;" href="about.php">About Us</a>
+        <a class="sideBar" style="margin-top: 20px;" href= "">Something</a>
+        <a class="sideBar" style="margin-top: 80px; color: red;" href= "game.php"><i class="fa fa-gamepad"></i>  Play Now</a>
+	  </div>
+    
+    <div class="main">
+    <h1 id="welcome">About Us</h1>
+
+    <p id="desc">Hello! We are from Group 4</p>
+
+    <!-- 450x360 -->
+    <!-- CODE IS REFERRED FROM W3SCHOOLS, TITLE AS HOW TO CREATE COLUMN CARDS, https://www.w3schools.com/howto/howto_css_column_cards.asp -->
+  
+
+    <div class="box">
+    <div class="container">
+        <div class="card">
+        <h3><img src="images/person.png" style="width:100%" id="pic"></h3>
+        <p>Muazzam Bin Murni</p>
+        <p>Student ID: 20FTT1443</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+        <h3><img src="images/person1.png" style="width:100%" id="pic"></h3>
+        <p>Batrisyia Najihah Binti Safri</p>
+        <p>Student ID: 20FTT1444</p>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="card">
+        <h3><img src="images/person2.png" style="width:100%" id="pic"></h3>
+        <p>Nur Amanina Aqilah Binti Awg Abdul Rahim</p>
+        <p>Student ID: 20FTT1456</p>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="card">
+        <h3><img src="images/person3.png" style="width:100%" id="pic"></h3>
+        <p>Khairunnisa' Asyiqin Binti Akandi</p>
+        <p>Student ID: 20FTT1461</p>
+        </div>
+    </div>
+
+    </div>
+    
+    
+
+
+
+
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<title>Introduction</title>
+<!-- LINK IS REFFERED FROM W3SCHOOLS.COM, TITLE AS HOW TO CREATE ICON BUTTONS, https://www.w3schools.com/howto/howto_css_icon_buttons.asp -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- BIZKID LOGO IS REFFERED FROM CANVA.COM, TITLE AS KIDS ZONE LOGO, https://www.canva.com/design/DAE-THhYchc/fJybr9urk51Ori8mV7F8Ag/edit?layoutQuery=kids+logo -->
+
+<html>
+
+<body>
+    <div class="wrap">
+		<div class="side">
+		<img src="images/bizkid2.png" id="bizkid">
+        <a class="sideBar" style="margin-top: 20px;" href="main.php">Home</a>
+        <a class="sideBar" style="margin-top: 20px;" href="about.php">About Us</a>
+        <a class="sideBar" style="margin-top: 20px;" href= "">Something</a>
+        <a class="sideBar" style="margin-top: 80px; color: red;" href= "game.php"><i class="fa fa-gamepad"></i> Play Now</a>
+	  </div>
+    
+    <div class="main">
+    <h1 id="welcome">Introduction</h1>
+
+    <!--DESCRIPTION IS REFFERRED FROM GOOGLE PLAY, TITLE AS MATCH GAME - PLAY&LEARN, https://play.google.com/store/apps/details?id=com.orange.memory.game.learn.kids&hl=en_US&gl=US  -->
+    <div class="box">
+        <div class="container">
+            <div class="card">
+            <p>BizKid is an educational, attractive and entertaining game for the whole family!</p>
+            <p>Are you ready to challenge your memory?</p>
+            <p>Have fun, learn and exercise your memory with this amazing game "Match the Pairs ".</p>
+            <p>By activating the pronunciation option, it becomes a valuable tool for children to learn to read, write and pronounce the name of objects.</p>
+            <p>Play against the clock and beat your own record</p>
+            
+            <p>Features:</p>
+            <p>★ SIMPLE and INTUITIVE design for the little ones!</p>
+            <p>★ Totally FREE: No content blocked!</p>
+            <p>★ Ideal for children to learn to recognize and pronounce the name of everyday objects.</p>
+            <p>★ The best way for childrens to exercise memory and concentration.</p>
+
+            <p>Modes:</p>
+            <p>★ Beginner</p>
+            <p>★ Advanced</p>
+            </div>
+
+        </div>
+    </div>
+    </div>
+    
+
+
+
+
+
+</body>
+</html>
